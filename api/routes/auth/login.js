1f83e565ea4d75e8f8070055600075e86106c361
HAMZA BOUCHGARA <bouchgarahamza@gmail.com>
2022-11-25 02:10:18 +0100

    add expiers date to the http only cookie

--- api/routes/auth/login.js
+++ api/routes/auth/login.js
@@ -52,7 +52,7 @@ router.post("/login", async (req, res) => {
     res.status(201)
         .cookie("accessToken", token, {
             httpOnly: true,
-            sameSite: "strict",
+            expires: Date.now() + 8640000,
         })
         .json({
             ok: true,
